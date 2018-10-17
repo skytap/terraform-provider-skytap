4f6095b0f35ae7619ccfef3f620a727a04c6e503
James Annesley <2913783+ja6a@users.noreply.github.com>
2018-10-17 15:59:36 +0100

    #13 reverting function name

--- skytap/resource_skytap_environment_test.go
+++ skytap/resource_skytap_environment_test.go
@@ -45,7 +45,7 @@ func testSweepSkytapEnvironment(region string) error {
 	return nil
 }
 
-func TestAccSkytapEnvironmentBasic(t *testing.T) {
+func TestAccSkytapEnvironment_Basic(t *testing.T) {
 	rInt := acctest.RandInt()
 
 	resource.Test(t, resource.TestCase{
